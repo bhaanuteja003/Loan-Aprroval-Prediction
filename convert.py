2cbaca511097fa5ee5f7271a31c7bdf80bdfe5db
bhaanuteja003 <bhanuteja.m.003@gmail.com>
2023-07-25 13:18:25 +0530

    hi

--- convert.py
+++ convert.py
@@ -3,6 +3,6 @@ import pandas as pd
 class Numerical:
     def converted(self,column,value):
         df_train = pd.read_csv('numerical.csv')
-        cat_data = pd.read_csv(r'Data Sets\loan_train.csv')
+        cat_data = pd.read_csv('loan_train.csv')
         num = df_train.loc[cat_data[cat_data[column]==value].index[0]][column]
         return num
